fbd6b7b35ab244dbdb0c5a5e5ec5e9830382cfbf
Jerome Caruso <jerome0caruso@gmail.com>
2022-01-31 12:07:10 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './app.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(/wordly/),
     filename: '_bundle.js'
   }
 };
